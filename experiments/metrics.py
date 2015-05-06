e9fe24790d4c942da58c7e8e2a9e0fbd8f53dc2e
Sergey <e-gress@mail.ru>
2015-05-06 16:45:30 +0500

    Added data for metrics

---
+++ experiments/metrics.py
@@ -0,0 +1,145 @@
+# coding=utf-8
+samples = {
+    "What is [OBJECT]?": [
+        "What is Alaska?",
+        "What is the fastest computer?"
+    ],
+    "Who was [OBJECT]?": [
+        "Who was Confucius?"
+    ],
+    "What is [OBJECT]'s [PROPERTY]?": [
+        "What is Alaska's area?"
+    ],
+    "What is [PROPERTY] of [OBJECT]?": [
+        "What is the area of Alaska?",
+        "What is the date of Boxing Day?",
+        "What is the population of Mexico?",
+        "What is the speed of the Mississippi River?",
+        "What is the size of Argentina?"
+    ],
+    "What's [PROPERTY] of [OBJECT]?": [
+        "What's the official language of Algeria?"
+    ],
+    "What is [PROPERTY] of [OBJECT]'s [PROPERTY]?": [
+        "What is the name of India's first satellite?",
+        "What is the name of King Arthur's sword?"
+    ],
+    "What are [PROPERTY] of [OBJECT]'s [PROPERTY]?": [
+        "What are the names of Odin's ravens?",
+
+    ],
+    "What was [PROPERTY] of [OBJECT]'s [PROPERTY]?": [
+        "What was the name of Captain Bligh's ship?"
+    ],
+    "What is [PROPERTY] of [PROPERTY] of [OBJECT]?" : [
+        "What is the name of first satellite of India?"
+    ],
+    "What is [PROPERTY] of [OBJECT]'s [PROPERTY]'s [PROPERTY]?": [
+        "What is date of death of Moscow's founder's brother?"
+    ],
+    "What's the oldest [PROPERTY] in [OBJECT]?": [
+        "What's the oldest capital city in the Americas?" # ?
+    ],
+    "What is the highest [PROPERTY] in [OBJECT]?": [
+        "What is the highest peak in Africa?" # ?
+    ],
+    "What is [PROPERTY] for [OBJECT]?": [
+        "What is the chemical symbol for nitrogen?",
+        "What is the telephone number for the University of Colorado?"
+    ],
+
+    # ------------------------------------
+
+    "What was [PROPERTY] by [OBJECT]?": [
+        "What was written by Shakespeare?"
+    ],
+
+    "What was [PROPERTY] by [OBJECT] in 1597?": [
+        "What was written by Shakespeare in 1597?"
+    ],
+    "What was [PROPERTY] by [OBJECT] between 1597 and 1600?":[
+        "What was written by Shakespeare between 1597 and 1600?"
+    ],
+
+    # ------------------------------------
+
+    "What [OBJECT] [PROPERTY]?":[
+        "What Shakespeare wrote?"
+    ],
+    "What does [OBJECT] [PROPERTY]?":[
+        "What does the ozone layer restricts?"
+    ],
+    "What [PROPERTY] does [OBJECT] use?": [
+        "What character encoding does Stanford CoreNLP use?" # ?
+    ],
+    "Where is [OBJECT] [PROPERTY]?":[
+        "Where is Garampani sanctuary located?",
+        " Where is Headquarters of UNO situated?"
+    ],
+
+    # ------------------------------------
+
+    "What is [PROPERTY] between [OBJECT_1] and [OBJECT_2]?": [
+        "What is the difference between a workgroup and a homegroup?"
+    ],
+
+
+
+
+    # --------------------------------
+    # --------------------------------
+    # --------------------------------
+    # --------------------------------
+    # --------------------------------
+
+
+    # "What is [PROPERTY] of [OBJECT] in miles?", #What is the height of Obama in miles?
+    # "what is [PROPERTY] to [OBJECT]?", # what is the distance to saturn?
+    #
+    #
+    # "How called [OBJECT]'s [PROPERTY]?", # How called India's first satellite?
+    # "How called [PROPERTY] by [OBJECT]?", # How called the book written by Brigham?
+    # "How [PROPERTY] is [OBJECT]?", # How old is Linus Torvalds?
+    # "How is [PROPERTY] in [OBJECT]?", # How is the weather in New York?
+    # "How [PROPERTY] is [OBJECT] to Maine?", # How far is London to Maine?
+    # "How many [PROPERTY] belong to [OBJECT]?", # How many Lok Sabha seats belong to Rajasthan?
+    # "How many [PROPERTY] does a [OBJECT] have?", # How many red blood cell does a normal human body have?
+    # "How much [PROPERTY] are there in [OBJECT]?", # How much districts are there in Punjab?
+    # "How many [PROPERTY] does [OBJECT] have?",
+    # "Where was [OBJECT] in London [PROPERTY]?", # Where was First International Peace Congress in London held?
+    #
+    # "What [PROPERTY] make up [OBJECT]?", # What colors make up a rainbow
+    # "What [PROPERTY] is used [OBJECT]?", # What currency is used in China?
+    # "What [OBJECT] contains [PROPERTY]?", # What part of your body contains the corpus callosum?
+    # "What [OBJECT] involved [PROPERTY]?", # What war involved the battle of Chapultepec?
+    # "What kind of [PROPERTY] are used in [OBJECT]?", # What kind of nuts are used in marzipan?
+    # "What [PROPERTY] does [OBJECT] play?", # What instrument does Max Roach play?
+    # "What [OBJECT] has [PROPERTY]?", # What religion has the most members?
+    # "What was [PROPERTY] of [PROPERTY] played by [OBJECT]?", # What was the name of the ball game played by the Mayans?
+    # "What [PROPERTY] do [OBJECT] use?", # What fuel do airplanes use?
+    #
+    #
+    # "What [PROPERTY] runs through [OBJECT]?", # What river runs through Liverpool?
+    # "What [OBJECT] do not have [PROPERTY]?", # What states do not have state income tax?
+    #
+    #
+    # "Which of [PROEPRTY] is used for [OBJECT]?", # Which of the gases is used for refrigeration?
+    # "Which [PROPERTY] were not involved in [OBJECT]?", # Which countries were not involved in the Second World War?
+    # "Who is [PROPERTY] of a [OBJECT]?", # Who is the developer of a telescope?
+    # "Who [PROPERTY] [OBJECT]?", # Who developed a telescope?
+    # "Who was the first [PROPERTY] of [OBJECT]?", # Who was the first Indian Chief of Army Staff of the Indian Army?
+    # "Who was the [PROPERTY] to be-elected to [OBJECT]?", # Who was the first Indian to be-elected to the British Parliament?
+    # "Who discovered [PROPERTY] of [OBJECT]?", # Who discovered magnetic field of electric current?
+    # "Who was [OBJECT] in 1981?", # Who was the US president in 1981?
+    # "Who is taller [OBJECT_1] or [OBJECT_2]?", # Who is taller Obama or Putin?
+    # "When was [OBJECT] [PROPERTY]?", # When was Alaska purchased?
+    #
+    #
+    # "What is the largest [ONE_WORD]?",
+    # "What is the largest [COMPLEX_WORD]?",
+    # "What is the largest [ONE_WORD] of Russia?",
+    # "What is the largest [COMPLEX_WORD] of Russia?",
+    # "What is Russia's largest railway station?",
+    #
+
+}
---
+++ experiments/samples.py
@@ -0,0 +1,131 @@
+# coding=utf-8
+samples = [
+
+    # "What is the area?",
+    # "What is area?",
+    # "What is the tree?",
+    # "What is tree?",
+    # "What is Alaska?",
+    # "What is UK?",
+    # "What is the railway station?",
+    # "What is railway station?",
+    # "What is the largest railway station?",
+    # "What is the largest railway station of Russia?",
+    # "What is Russia's largest railway station?",
+    # "What is Russia's population?",
+    # "What is stands FFC?",
+    # "What is official language of Alaska?",
+    # "What is the name of the science that studies insects?",
+    # "What is the name of India's first satellite?",
+    # "What is the name of the first satellite of India?",
+    # "What was written by Shakespeare?",
+    # "What was written by Shakespeare in 1597?",
+    # "What was written by Shakespeare between 1597 and 1600?",
+    # "What did Shakespeare write?",
+    # "What was installed during Eleventh Antarctic Expedition?",
+    # "What does the ozone layer restricts?",
+    # "What character encoding does Stanford CoreNLP use?",
+    # "Which events happened in 1949?",
+    # "What is the distance between Alaska and Moscow?",
+    # "What is the square root of pi?",
+    # "What is 160 times Pi?",
+    # "What is the height of Obama in miles?",
+    # "What is the difference between a workgroup and a homegroup?",
+    # "what is the distance to saturn?",
+    #
+    # "How called the science that studies insects?",
+    # "How called India's first satellite?",
+    # "How called the book written by Brigham?",
+    # "How old is Linus Torvalds?",
+    # "How is the weather in New York?",
+    # "How far is London to Maine?",
+    # "How many Lok Sabha seats belong to Rajasthan?",
+    # "How many red blood cell does a normal human body have?",
+    # "How many red blood cells does the bone marrow produce every second?",
+    # "How many times has Brazil won the World Cup Football Championship?",
+    # "How much districts are there in Punjab?",
+    # "How many legs does a male have?",
+    # "How much is 57.5 kg in stones?",
+    # "Where is Garampani sanctuary located?",
+    # "Where does Coral Reefs in India can be found?",
+    # "Where is Headquarters of UNO situated?",
+    # "Where was First International Peace Congress in London held?",
+    # "Where was stored memories?",
+    # "Where are memories stored?",
+    # "Which country has the largest deposits of mica in the world?",
+    # "Which language was patronised by the rules of Delhi Sultanate?",
+    # "Which country leads in the production of rubber?",
+    # "Which is the most irrigated State in India?",
+    # "Which of the animals was not native to India?",
+    # "Which of the gases is used for refrigeration?",
+    # "Which countries were not involved in the Second World War?",
+    # "Who is the developer of a telescope?",
+    # "Who developed a telescope?",
+    # "Who was the first Indian Chief of Army Staff of the Indian Army?",
+    # '''Who said: "Love is like a war, Easy to begin Hard to end"?''',
+    # "Who was the first Indian to be-elected to the British Parliament?",
+    # "Who discovered magnetic field of electric current?",
+    # "Who is Doctor Who?",
+    # "Who was the US president in 1981?",
+    # "Who is taller Obama or Putin?",
+    # "When was conducted the first human heart transplant operation?",
+    # "When was conducted the first human heart transplant operation conducted by Dr. Christian Bernard?",
+    # "When the World Red Cross is celebrated?",
+    # "When the World Red Cross and Red Crescent Day are celebrated?",
+    # "When did Georgia become a member of UNO?",
+    # "When did Georgia, Uzbekistan and Turkmenistan become members of UNO?",
+    # "When was hockey introduced?",
+    # "When was hockey introduced in the Asian Games?",
+    # "When did crusaders reach Jerusalem and capture it during the first crusade?",
+    # "When was Alaska purchased?",
+
+    "What is date of death of Moscow's founder's brother?",
+
+    # Связи: A => B
+    #        A and B
+    #        A or B
+    "What are the names of Odin's ravens?",                 # o:Odin, p:ravens -> o:*, p:name
+    "What colors make up a rainbow?",                       # o:Rainbow, p:contained colors
+    "In what book can I find the story of Alladin?",        # o:Alladin, p:written in THIS book
+    "What currency is used in China?",                      # o:China, p:currency
+    "What part of your body contains the corpus callosum?", #
+    "What does Salk vaccine prevent?",                      #
+    "What war involved the battle of Chapultepec?",         # o:battle of Chapultepec, p:part of war
+    "What kind of nuts are used in marzipan?",              #
+    "What instrument does Max Roach play?",                 # o:Max Roach, p:instrument
+    "What's the official language of Algeria?",             # o:Algeria, p:official language
+    "What letter appears on the cold-water tap in Spain?",  #
+    "What is the name of King Arthur's sword?",             # o:King Arthur, p:sword -> o:$, p:name
+    "What are some fragrant white climbing roses?",         #
+    "What is the fastest computer?",                        #
+    "What religion has the most members?",                  #
+    "What was the name of the ball game played by the Mayans?",
+    "What fuel do airplanes use?",                          # o:airplines, p:fuel
+    "What is the chemical symbol for nitrogen?",            # o:nitrogen, p:chemical symbol
+    '''How do you say "Grandma" in Irish?''',               #
+    "What was the name of Captain Bligh's ship?",           # o:Capitan Blight, p:admiral of -> o:$, p:name
+    "What's the singular of dice?",                         # o:dice, p:singular of
+
+    # "Who was Confucius?",                                   # o:Confucius
+    # "What are the major companies that are part of Dow Jones?",
+    # "Who was the first Russian astronaut to do a spacewalk?", #
+    # "What was Queen Victoria's title regarding India?",     #
+    # "What's the oldest capital city in the Americas?",      #
+    # "What country borders the most others?",                #
+    # "What is the highest peak in Africa?",                  #
+    # "What river runs through Liverpool?",                   #
+    # "What states do not have state income tax?",
+    # "What is the telephone number for the University of Colorado?",
+    # "About how many soldiers died in World War II?",
+    # "What is the date of Boxing Day?",
+    # "How long was Mao's 19930s Long March?",
+    # "How much did a McDonald's hamburger cost in 1963?",
+    # "Where does Shanghai rank among world cities in population?",
+    # "What is the population of Mexico?",
+    # "What was the average life expectancy during the Stone Age?",
+    # "What fraction of beaver's life is spent swimming?",
+    # "What is the speed of the Mississippi River?",
+    # "How fast must a spacecraft travel to escape Earth's gravity?",
+    # "What is the size of Argentina?",
+    # "How many pounds are there in a stone?"
+]
